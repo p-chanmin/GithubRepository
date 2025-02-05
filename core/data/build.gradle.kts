adf80fae8fccf2b2149e87bd9bc67587fb3b9f43
p-chanmin <pc_m111@naver.com>
2025-02-06 04:13:50 +0900

    build: okhttp, retrofit gradle 설정

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -8,6 +8,10 @@ android {
 }
 
 dependencies {
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+
     implementation(project(":core:model"))
     implementation(project(":core:data-api"))
 }
